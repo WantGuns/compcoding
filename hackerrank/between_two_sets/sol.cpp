8a110e0c630e8732e1663b00da34ab4c5939dfb8
wantguns <therealgunwant@gmail.com>
2019-07-19 17:51:43 +0530

    error 1 is solved lmao

--- hackerrank/between_two_sets/sol.cpp
+++ hackerrank/between_two_sets/sol.cpp
@@ -6,8 +6,9 @@
 using namespace std;
 
 int main(){
-  int n, m, a[n], b[m], temp, num[1000], flag = 1;
+  int n, m, temp, num[1000], flag = 1;
   cin>>n>>m;
+  int  a[n], b[m];
   for (int i = 0; i < n ; i++) {
     cin >> a[i];
   }
