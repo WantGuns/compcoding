a1255a9947f7a27430903f8ffef86352b35110d5
wantguns <therealgunwant@gmail.com>
2019-08-25 16:10:46 +0530

    trying to correctly implement mod this time.

--- codechef/keteki_2/keteki2c/sol.cpp
+++ codechef/keteki_2/keteki2c/sol.cpp
@@ -1,13 +1,11 @@
 //wip for bigger numbers. works fine for smaller ones.
-
-
 #include <iostream>
 using namespace std;
 
 #define ll long long int
 
 int main() {
-  ll n, mod = 1000000007;
+  ll n, mod = 1000000007, test = 1;
   cin>>n;
   ll a[n], b[n], count=0;
   for (int i = 0; i < n; i++) b[i] = 1;
@@ -15,8 +13,11 @@ int main() {
   cin >> a[i];
   for (int i = 0; i < n; i++)
     for (int j = 0; j < n; j++)
-      if (j != i)
-        b[i] = (b[i]*a[j])%mod;
+      if (j != i) {
+        test = 1;
+        test = (test * a[i])%mod;
+        b[i] = (test * b[j])%mod;
+      }
   for (int h = 0; h < n; h++)
     for (int i = 1; i <= b[h]; i++)
         if (b[h] % i == 0){
